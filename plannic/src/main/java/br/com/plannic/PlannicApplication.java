1cb15c5c5cc4d9ab239d9c471eb56e817a9753ea
Mateus Santana <mateusjoaquim@live.com>
2021-01-17 14:38:53 -0300

    inicio projeto

---
+++ plannic/src/main/java/br/com/plannic/PlannicApplication.java
@@ -0,0 +1,13 @@
+package br.com.plannic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlannicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlannicApplication.class, args);
+	}
+
+}
---
+++ plannic/src/test/java/br/com/plannic/PlannicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.plannic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlannicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
