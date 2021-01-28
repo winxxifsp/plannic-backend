d9d0d41d9296d5d15bd757acdc6275fade0c44c9
Rebeca Souza <rebecasza.santos@gmail.com>
2021-01-28 00:15:13 -0300

    ajustando parametros para conexao do banco

--- src/main/java/br/com/plannic/PlannicApplication.java
+++ src/main/java/br/com/plannic/PlannicApplication.java
@@ -2,8 +2,10 @@ package br.com.plannic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"br.com.plannic.service"})
 public class PlannicApplication {
 
 	public static void main(String[] args) {
