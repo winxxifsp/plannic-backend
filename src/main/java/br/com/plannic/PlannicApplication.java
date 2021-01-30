85a10b628a4b5469ff0270a85712b90ee984bd1c
Rebeca Souza <rebecasza.santos@gmail.com>
2021-01-30 20:34:14 -0300

    Removing test

--- src/main/java/br/com/plannic/PlannicApplication.java
+++ src/main/java/br/com/plannic/PlannicApplication.java
@@ -7,11 +7,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PlannicApplication {
-
-	private static Logger logger = Logger.getLogger(UsuarioService.class);
-
+	
 	public static void main(String[] args) {
-		logger.info("ttttttttttttteste");
 		SpringApplication.run(PlannicApplication.class, args);
 	}
 
