6754a81b43ec97019af2bbfe4c8088b763e93543
Rebeca Souza <rebecasza.santos@gmail.com>
2021-02-01 20:37:52 -0300

    Adding logs info and error

--- src/main/java/br/com/plannic/controller/AuthenticateController.java
+++ src/main/java/br/com/plannic/controller/AuthenticateController.java
@@ -3,6 +3,7 @@ package br.com.plannic.controller;
 
 import br.com.plannic.model.AuthRequest;
 import br.com.plannic.util.JwtUtil;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -22,14 +23,18 @@ public class AuthenticateController {
         return "WINXS IFSP";
     }
 
+    private static Logger logger = Logger.getLogger(AuthenticateController.class);
+
     @PostMapping("/authenticate")
     public String generateToken(@RequestBody AuthRequest authRequest) throws Exception {
         try {
+            logger.info("Autenticando o usuário.");
             authenticationManager.authenticate(
                     new UsernamePasswordAuthenticationToken(authRequest.getEmail(), authRequest.getPassword())
             );
         } catch (Exception ex) {
-            throw new Exception("erro na autenticação.");
+            logger.error("Erro na autenticação do usuário");
+            throw new Exception("Erro na autenticação do usuário.");
         }
         return jwtUtil.generateToken(authRequest.getEmail());
     }
