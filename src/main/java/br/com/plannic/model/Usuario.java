fcaa092315bf4c26ba8b8d3c00e539021609bd2b
Mateus Santana <mateusjoaquim@live.com>
2021-02-02 17:36:55 -0300

    ajuste no usuario

--- src/main/java/br/com/plannic/model/Usuario.java
+++ src/main/java/br/com/plannic/model/Usuario.java
@@ -4,10 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
@@ -16,9 +13,10 @@ import javax.persistence.Table;
 @Table(name = "usuario")
 public class Usuario {
 
+    @Id
     @GeneratedValue
     private int id;
-    @Id
+    @Column(unique =true)
     private String email;
     private String password;
     private String nome;
