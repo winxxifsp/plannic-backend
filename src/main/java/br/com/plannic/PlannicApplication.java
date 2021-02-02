592fe4afce2b83b7fe608a1cd0948d6f0c4f6eac
Rebeca Souza <rebecasza.santos@gmail.com>
2021-02-01 22:41:02 -0300

    Fixing timezone

--- src/main/java/br/com/plannic/PlannicApplication.java
+++ src/main/java/br/com/plannic/PlannicApplication.java
@@ -1,5 +1,7 @@
 package br.com.plannic;
 
+import br.com.plannic.service.UsuarioService;
+import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
