73ef7227177f600c392b23f1f9440d5f95151e1b
Rebeca Souza <rebecasza.santos@gmail.com>
2021-02-05 00:50:46 -0300

    env fix

--- src/test/java/br/com/plannic/PlannicApplicationTests.java
+++ src/test/java/br/com/plannic/PlannicApplicationTests.java
@@ -3,11 +3,11 @@ package br.com.plannic;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class PlannicApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class PlannicApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
