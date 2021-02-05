4913a78eccda1a834b45c142b7ee85b34ada9fa2
Rebeca Souza <rebecasza.santos@gmail.com>
2021-02-05 00:30:55 -0300

    Fixing env

--- src/main/java/br/com/plannic/PlannicApplication.java
+++ src/main/java/br/com/plannic/PlannicApplication.java
@@ -1,12 +1,22 @@
 package br.com.plannic;
 
+import br.com.plannic.controller.AuthenticateController;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class PlannicApplication {
 
-	public static void main(String[] args) {SpringApplication.run(PlannicApplication.class, args);
+	private static Logger logger = Logger.getLogger(AuthenticateController.class);
+
+	public static void main(String[] args) {
+		System.setProperty("DATABASE_PASSWORD", System.getenv("DATABASE_PASSWORD"));
+		System.setProperty("DATABASE_USERNAME", System.getenv("DATABASE_USERNAME"));
+		System.setProperty("DATABASE_URL", System.getenv("DATABASE_URL"));
+		PropertyConfigurator.configure("src/main/resources/log4j.properties");
+		SpringApplication.run(PlannicApplication.class, args);
 	}
 
 }
